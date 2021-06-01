3d224ac9e1f46d9c737359bdd908bb9c1ffbc242
burakcy <yalcin.burak01@gmail.com>
2021-06-02 01:18:40 +0300

    gelir eklendi

---
+++ Entity/Concrete/Gelir.cs
@@ -0,0 +1,21 @@
+﻿using MuhasebeApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    class Gelir : IEntity
+    {
+        public int Id { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public decimal AlinanTutar { get; set; }
+        public DateTime Tarih { get; set; }
+        public int MalzemeId { get; set; }
+        public int Adet { get; set; }
+        public string OdemeSekli { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
